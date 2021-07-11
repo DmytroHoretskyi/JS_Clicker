1d566fffb203f88128730590c5beabec03b991ec
DmytroHoretskyi <serwserw16@gmail.com>
2021-07-11 23:35:33 +0300

    first code of my clicker

---
+++ main.js
@@ -0,0 +1,33 @@
+let h1Counter = document.getElementById('counter');
+let buttonId = document.getElementById('clickButton');
+let upgradeButton = document.getElementById('upgradeButton');
+let cauntionMessage = document.getElementById('siteBlocker');
+let popUp = document.getElementById('popUp')
+let popUpError = document.getElementById('close')
+let popUpText = document.getElementById('mistake')
+let counter = 0;
+let upgrade0 = 1;
+
+popUpError.addEventListener('click',function (){
+   cauntionMessage.classList.add('hidden');
+   popUp.classList.add('hidden');
+})
+upgradeButton.addEventListener('click', function (){
+   if (10 > counter){
+      popUp.classList.remove('hidden');
+      cauntionMessage.classList.remove('hidden')
+   popUpText.textContent = "Alert! you need 10";
+
+   }
+   else {
+      counter = counter - 10;
+      upgrade0 = upgrade0 + 1;
+      h1Counter.textContent = 'click counter: ' + counter;
+   }
+
+});
+
+buttonId.addEventListener('click', function (){
+   counter = counter + upgrade0;
+   h1Counter.textContent = 'click counter: '+ counter;
+});
