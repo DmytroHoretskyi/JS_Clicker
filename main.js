bfda7c87e3245b22a641035396099a60a14b7d2e
DmytroHoretskyi <serwserw16@gmail.com>
2021-07-14 22:54:53 +0300

    some upgrades and try to make sessionStorage

--- main.js
+++ main.js
@@ -1,33 +1,36 @@
 let h1Counter = document.getElementById('counter');
 let buttonId = document.getElementById('clickButton');
 let upgradeButton = document.getElementById('upgradeButton');
-let cauntionMessage = document.getElementById('siteBlocker');
+let cautionMessage = document.getElementById('siteBlocker');
 let popUp = document.getElementById('popUp')
 let popUpError = document.getElementById('close')
 let popUpText = document.getElementById('mistake')
-let counter = 0;
-let upgrade0 = 1;
+let counter = parseInt(sessionStorage.getItem('counter'));
+let upgrade0 = parseInt(sessionStorage.getItem('upgrade'));
 
 popUpError.addEventListener('click',function (){
-   cauntionMessage.classList.add('hidden');
+   cautionMessage.classList.add('hidden');
    popUp.classList.add('hidden');
 })
-upgradeButton.addEventListener('click', function (){
-   if (10 > counter){
-      popUp.classList.remove('hidden');
-      cauntionMessage.classList.remove('hidden')
-   popUpText.textContent = "Alert! you need 10";
 
+upgradeButton.addEventListener('click', function (){
+let upgradeStocks = 10;
+   if (upgradeStocks > counter){
+      popUp.classList.remove('hidden');
+      cautionMessage.classList.remove('hidden')
+   popUpText.textContent = `Alert! you need ${upgradeStocks} clicks`;
    }
    else {
-      counter = counter - 10;
-      upgrade0 = upgrade0 + 1;
+      counter -= upgradeStocks;
+      upgrade0 += 1;
       h1Counter.textContent = 'click counter: ' + counter;
    }
-
 });
 
 buttonId.addEventListener('click', function (){
-   counter = counter + upgrade0;
+   counter += upgrade0;
    h1Counter.textContent = 'click counter: '+ counter;
+   sessionStorage.setItem('counter', counter);
+   alert(typeof (sessionStorage.getItem('counter')))
+   sessionStorage.setItem('upgrade', upgrade0)
 });
