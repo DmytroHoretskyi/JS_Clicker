23bac02d6a1b2fc9b08e433abaf231154b921fc2
Joel D <imaginethere.snoie@gmail.com>
2021-07-15 09:51:45 +0300

    Fix Nan in sessionStorage

--- main.js
+++ main.js
@@ -1,36 +1,43 @@
-let h1Counter = document.getElementById('counter');
-let buttonId = document.getElementById('clickButton');
-let upgradeButton = document.getElementById('upgradeButton');
-let cautionMessage = document.getElementById('siteBlocker');
-let popUp = document.getElementById('popUp')
-let popUpError = document.getElementById('close')
-let popUpText = document.getElementById('mistake')
-let counter = parseInt(sessionStorage.getItem('counter'));
-let upgrade0 = parseInt(sessionStorage.getItem('upgrade'));
+const h1Counter = document.getElementById('counter');
+const buttonId = document.getElementById('clickButton');
+const upgradeButton = document.getElementById('upgradeButton');
+const cautionMessage = document.getElementById('siteBlocker');
+const popUp = document.getElementById('popUp');
+const popUpError = document.getElementById('close');
+const popUpText = document.getElementById('mistake');
 
-popUpError.addEventListener('click',function (){
+let userData = {
+   clickCount: 0,
+   upgradeCount: 1,
+   upgradeStocks: 10,
+};
+
+if ('userData' in sessionStorage) {
+   userData = JSON.parse(sessionStorage.getItem('userData'));
+}
+
+h1Counter.textContent = 'click counter: ' + userData.clickCount;
+
+popUpError.addEventListener('click',() => {
    cautionMessage.classList.add('hidden');
    popUp.classList.add('hidden');
-})
+});
 
-upgradeButton.addEventListener('click', function (){
-let upgradeStocks = 10;
-   if (upgradeStocks > counter){
+upgradeButton.addEventListener('click', () => {
+   if (userData.upgradeStocks > userData.clickCount){
       popUp.classList.remove('hidden');
       cautionMessage.classList.remove('hidden')
-   popUpText.textContent = `Alert! you need ${upgradeStocks} clicks`;
-   }
-   else {
-      counter -= upgradeStocks;
-      upgrade0 += 1;
-      h1Counter.textContent = 'click counter: ' + counter;
+      popUpText.textContent = `Alert! you need ${userData.upgradeStocks} clicks`;
+
+   } else {
+      userData.clickCount -= userData.upgradeStocks;
+      userData.upgradeCount += 1;
+      h1Counter.textContent = 'click counter: ' + userData.clickCount;
    }
 });
 
-buttonId.addEventListener('click', function (){
-   counter += upgrade0;
-   h1Counter.textContent = 'click counter: '+ counter;
-   sessionStorage.setItem('counter', counter);
-   alert(typeof (sessionStorage.getItem('counter')))
-   sessionStorage.setItem('upgrade', upgrade0)
+buttonId.addEventListener('click', () => {
+   userData.clickCount += userData.upgradeCount;
+   h1Counter.textContent = 'click counter: '+ userData.clickCount;
+   sessionStorage.setItem('userData', JSON.stringify(userData));
 });
